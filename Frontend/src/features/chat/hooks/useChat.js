dfaf268b6e630632c6dbf8df7aa4e7489dd5da80
Akshat Bamania <akshat.bamania23@gmail.com>
2026-03-30 23:51:22 +0530

    feat: small changes

--- Frontend/src/features/chat/hooks/useChat.js
+++ Frontend/src/features/chat/hooks/useChat.js
@@ -66,7 +66,7 @@ export function useChat() {
                 messages: formattedMessages,
             }))
         }
-        dispatch(setCurrentChatId(chatId ))
+        dispatch(setCurrentChatId(chatId))
     }
 
     return {
